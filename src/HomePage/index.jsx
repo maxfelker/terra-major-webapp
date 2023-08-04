38e8c9a5de6e95e9d1fd715845383fd42e20767f
MW Felker <mwadefelker@gmail.com>
2023-08-04 17:15:07 -0400

    Added home page

--- src/HomePage/index.jsx
+++ src/HomePage/index.jsx
@@ -1,25 +1,31 @@
 import { Link } from "react-router-dom";
+import styles from "./styles.module.css";
 
 export default function HomePage() {
   const version = localStorage.getItem('build-version');
 
   return (
     <>
-      <h1>Terra Major v{version}</h1>
+      <div className={styles.hero}>
+        <div className={styles.heroContent}>
+           <h1>Terra Major</h1>
+        </div>
+      </div>
+    
+      <div className={styles.content}>
+        <h2>v{version} - Single player persistent sandbox</h2>
+        <p>Terra Major is a free-to-play, cross-platform game about interstellar colonization.
+          Open alpha available via WebGL, no installation or payment required.</p>
+        <ul>
+          <li>Players can create and manage their characters</li>
+          <li>Each character has thier own persistent sandbox</li>
+          <li>Players can mine Omnium ore and redeem at the Refinery</li>
+          <li>Players can build structures in their sandbox</li>
+        </ul>
+        <p>Commonwealth citizens can <Link to="/login"><button>Warp to Surface</button></Link></p>
+        <p>All civillians must <Link to="/sign-up">apply for Citizenship!</Link></p>
 
-      <p>Returning users <Link to="/login">login</Link></p>
-      
-      <p>New users <Link to="/sign-up">create account</Link></p>
-
-      <p>Terra Major is a free-to-play, cross-platform sci-fi game that allows players to build colonies, gather resources, and challenge other players in a real-time, persistent game world.</p>
-
-      <h2>Infinite Single Player Sandbox</h2>
-      <ul>
-        <li>Base building and territory control</li>
-        <li>Resource management</li>
-        <li>Open world exploration</li>
-        <li>Character skills and growth</li>
-      </ul>
+      </div>
     </>
   )
 }
