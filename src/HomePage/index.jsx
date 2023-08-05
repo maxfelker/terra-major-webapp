f67f2913ad1a547fedb49cb561a033ab5d74d680
Max Felker <official@maxfelker.com>
2023-08-04 23:49:42 -0400

    Added some wide angle and iphone lanscape tweaks

--- src/HomePage/index.jsx
+++ src/HomePage/index.jsx
@@ -10,22 +10,26 @@ export default function HomePage() {
     <>
       <div className={styles.hero}>
         <div className={styles.heroContent}>
+          <div className={styles.content}>
            <h1>Terra Major</h1>
+          </div>
         </div>
       </div>
     
       <div className={styles.content}>
-        <p>Terra Major is a free-to-play game about interstellar colonization. The 
-          open alpha available via WebGL, no installation or payment required.</p>
+        <p>Terra Major is a free-to-play game about interstellar colonization. 
+          No installation or payment required. The current version v{version} is an open alpha 
+          and we are looking for early adopters.</p>
 
         <p><Link to="/login"><button>Play Now</button></Link></p>
-        <p>All civillians must <Link to="/sign-up">apply for Citizenship!</Link></p>
 
       </div>
 
       <div className={styles.hero} style={{backgroundImage:"url("+art3+")"}}>
         <div className={styles.heroContent}>
-           <h1>It's your world</h1>
+          <div className={styles.content}>
+            <h1>It's your world</h1>
+          </div>
         </div>
       </div>
 
@@ -42,16 +46,18 @@ export default function HomePage() {
 
       <div className={styles.hero} style={{backgroundImage:"url("+art2+")"}}>
         <div className={styles.heroContent}>
+          <div className={styles.content}>
            <h1>No limits</h1>
+           </div>
         </div>
       </div>
 
       <div className={styles.content}>
         <h2>Go over them mountains</h2>
-        <p>Content here about why it's so cool</p>
+        <p>Each experience is dynamically generated and the entire game world is playable.</p>
         <ul>
           <li>No artificial boundaries</li>
-          <li>The entire game world is playable</li>
+          <li>Unique experience for every player</li>
         </ul>
         <p><Link to="/login"><button>Explore Terra Major</button></Link></p>
       </div>
